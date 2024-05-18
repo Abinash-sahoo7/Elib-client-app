7ff5e4695e17028cb6de0bdbc6a0e80e2ea82a84
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2024-05-18 15:48:29 +0530

    add footer component

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -4,7 +4,7 @@ import BookCard from './BookCard'
 
 function BookList({books} : {books : Book[] }) {
   return (
-    <div className='grid grid-cols-3 gap-3 md:grid-cols-3 max-w-7xl mx-auto'>
+    <div className='grid grid-cols-3 gap-3 md:grid-cols-3 max-w-7xl mx-auto mb-8'>
       {
         books.map((book) => (
           <BookCard key={book._id} book={book} />
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t-2 p-5 text-center'>
+        @ 2024 Abinash#$09 | All Books rights are reserved..
+    </footer>
+  )
+}
+
+export default Footer
